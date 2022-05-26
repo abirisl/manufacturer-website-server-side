ddaab68b7ce728722683c8ad660d2f8f8b4ee7de
abir islam <mhabir979292@gmail.com>
2022-05-26 22:23:13 +0600

    manageproduct delete api and add product api make

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.09lai.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJwt(req,res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unAuthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCES_TOKEN_SECRET, function(err,decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+    
+}
+
 async function run() {
     try {
       await client.connect();
@@ -29,6 +46,37 @@ async function run() {
           res.send(products)
       });
 
+      app.get('/user', verifyJwt, async(req,res)=>{
+          const users = await userCollection.find().toArray()
+          res.send(users)
+      });
+
+      app.get('/admin/:email', async(req,res) =>{
+          const email = req.params.email;
+          const user = await userCollection.findOne({email: email});
+          const isAdmin = user.role === 'admin';
+          res.send({admin: isAdmin})
+      })
+
+      app.put('/user/admin/:email', verifyJwt, async(req,res) =>{
+        const email = req.params.email;
+        const requesting = req.decoded.email;
+        const requestingAccount = await userCollection.findOne({email: requesting});
+        console.log(requestingAccount)
+        if(requestingAccount.role === 'admin'){
+            const filter ={ email: email };
+        const updateDoc = {
+            $set: {role: 'admin'},
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+
+        }
+        else{
+            res.status(403).send({message: 'forbidden'})
+        }
+
+    })
 
       app.put('/user/:email', async(req,res) =>{
           const email = req.params.email;
@@ -39,7 +87,7 @@ async function run() {
               $set: user,
           };
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          const token = jwt.sign({email: email}, process.env.ACCES_TOKEN_SECRET, {expiresIn: '1h' })
+          const token = jwt.sign({email: email}, process.env.ACCES_TOKEN_SECRET)
           res.send({result, token});
 
       })
@@ -64,6 +112,20 @@ async function run() {
          res.send(result)
      });
 
+     app.post('/addproduct', async(req, res) =>{
+         const newproduct = req.body;
+         const result = await manufacturerCollection.insertOne(newproduct);
+         res.send(result)
+
+     });
+
+     app.get('/manageproduct', async (req, res) => {
+            const query = {};
+            const cursor = manufacturerCollection.find(query);
+            const manageorder = await cursor.toArray();
+            res.send(manageorder);
+    })
+
      app.get('/review', async(req,res) =>{
          const query = {};
          const cursor = reviewCollection.find(query);
@@ -72,11 +134,18 @@ async function run() {
 
      })
 
-     app.get('/order', async(req,res) =>{
+     app.get('/order', verifyJwt,  async(req,res) =>{
          const email = req.query.customerEmail;
-         const query = {customerEmail: email};
-        const order = await orderCollection.find(query).toArray();
-         res.send(order);
+         const decodedEmail = req.decoded.email;
+         if(email === decodedEmail){
+            const query = {customerEmail: email};
+            const order = await orderCollection.find(query).toArray();
+             res.send(order);
+         }
+         else{
+             return res.status(403).send({message: 'forbidden access'})
+         }
+        
      });
 
      app.get('/allorder', async(req,res) =>{
@@ -88,6 +157,14 @@ async function run() {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
         const result = await orderCollection.deleteOne(query);
+        res.send(result);
+
+    });
+
+    app.delete('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await manufacturerCollection.deleteOne(query);
         res.send(result);
 
     });
