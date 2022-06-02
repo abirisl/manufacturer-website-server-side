a37ce4bf2802353ffa2e5656ecfbc0c6ceb169a7
abir islam <mhabir979292@gmail.com>
2022-06-02 08:24:18 +0600

    msg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -7,7 +7,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors({origin: 'https://manufacturer-website-87c0c.web.app'}));
 app.use(express.json());
 
 
