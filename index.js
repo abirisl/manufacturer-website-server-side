052282f11424e696f3a0ee37b3213d644d184aeb
abirisl <mhabir979292@gmail.com>
2023-02-03 16:09:44 +0600

    update server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(cors({origin: 'https://manufacturer-website-87c0c.web.app'}));
 app.use(express.json());
 
-
+// user and pass
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.09lai.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
